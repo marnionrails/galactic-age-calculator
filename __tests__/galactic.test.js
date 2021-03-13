bc6b4f6fcb449c7e431e5abf15275ceb40394f34
marnionrails <37088724+marnionrails@users.noreply.github.com>
2021-03-13 11:07:32 -0500

    add first spec to check if input age is valid number

---
+++ __tests__/galactic.test.js
@@ -0,0 +1,7 @@
+import Galactic from './..src/galactic.js';
+
+describe('Galactic', () => {
+
+  test('should return a string asking for a number, given any other type of input is entered');
+  expect().toEqual("please enter a valid number for age");
+});
