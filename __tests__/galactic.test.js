4cd640912dbdf6c474653a01bbc54ab607bb9c0d
marnionrails <37088724+marnionrails@users.noreply.github.com>
2021-03-13 12:18:23 -0500

    add spec to return user age in Mercury years

--- __tests__/galactic.test.js
+++ __tests__/galactic.test.js
@@ -1,11 +1,16 @@
 import Galactic from './../src/galactic.js';
 
 describe('Galactic', () => {
-
+    
     test('should return a string asking for a number, given any other type of input is entered', () => {
     const userAge = new Galactic("Marni");
     userAge.isNumber();
     const result = userAge.dialogue;
     expect(result).toEqual("please enter a valid number for age");
+  });
+
+    test('should return user age in Mercury years', () => {
+    userAge.onMercury()
+    expect().toEqual(116.3)
   });
 });
