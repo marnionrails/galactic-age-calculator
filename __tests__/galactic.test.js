c1c254bcc3e69a380d2dd26d0a117f51e459717c
marnionrails <37088724+marnionrails@users.noreply.github.com>
2021-03-13 11:46:47 -0500

    achieve good fail for number input spec

--- __tests__/galactic.test.js
+++ __tests__/galactic.test.js
@@ -1,7 +1,11 @@
-import Galactic from './..src/galactic.js';
+import Galactic from './../src/galactic.js';
 
 describe('Galactic', () => {
 
-  test('should return a string asking for a number, given any other type of input is entered');
-  expect().toEqual("please enter a valid number for age");
+    test('should return a string asking for a number, given any other type of input is entered', () => {
+    const userAge = new Galactic("Marni");
+    userAge.isNumber();
+    const result = userAge.dialogue;
+    expect(result).toEqual("please enter a valid number for age");
+  });
 });
---
+++ src/galactic.js
@@ -0,0 +1,11 @@
+export default class Galactic {
+  constructor (user_age) {
+    this.age = user_age;
+    this.dialogue = '';
+  }
+
+  isNumber () {
+    
+    return this.dialogue;
+  }
+}
